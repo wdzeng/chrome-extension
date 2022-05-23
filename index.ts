cce2c2014ce865e4bcf271d4be44391a079e35f1
Hyperbola <me@hyperbola.me>
2022-05-23 19:00:49 +0800

    add types and fix some bugs

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-import * as core from '@actions/core'
-import axios, { AxiosError } from 'axios'
+
 import fs from 'fs/promises'
 import path from 'path'
+import * as core from '@actions/core'
+import axios, { AxiosError, AxiosRequestHeaders, AxiosResponse } from 'axios'
+import { ItemPublishResponseData, ItemResponseData, OAuth2TokenResponse, UploadState } from './types.js'
 
 // https://developer.chrome.com/docs/webstore/using_webstore_api/
 
 async function generateJwtToken(clientId: string, clientSecret: string, refreshToken: string): Promise<string> {
   // https://developers.google.com/identity/protocols/oauth2/web-server#httprest_1
   core.info('Start to refresh access token.')
-  const response = await axios.post('https://oauth2.googleapis.com/token', {
+  const response: AxiosResponse<OAuth2TokenResponse> = await axios.post('https://oauth2.googleapis.com/token', {
     client_id: clientId,
     client_secret: clientSecret,
     refresh_token: refreshToken,
@@ -17,25 +19,57 @@ async function generateJwtToken(clientId: string, clientSecret: string, refreshT
 
   const accessToken = response.data.access_token
 
-  core.debug('Access token: ' + accessToken)
+  core.debug('Got access token: ' + accessToken)
   core.debug(JSON.stringify(response.data))
   core.info('Access token refreshed.')
 
   return accessToken
 }
 
-async function updateExtension(extId: string, zipPath: string, token: string): Promise<void> {
+async function updatePackage(extId: string, zipPath: string, token: string): Promise<void> {
+  let url: string
+  let headers: AxiosRequestHeaders
+  let response: AxiosResponse<ItemResponseData>
+  let uploadState: UploadState
+
   // https://developer.chrome.com/docs/webstore/using_webstore_api/#uploadexisitng
+  // https://developer.chrome.com/docs/webstore/using_webstore_api/#checkstatus
+
   core.info('Start to update extension package.')
 
-  const url = `https://www.googleapis.com/upload/chromewebstore/v1.1/items/${extId}?uploadType=media`
+  url = `https://www.googleapis.com/upload/chromewebstore/v1.1/items/${extId}?uploadType=media`
   const body = await fs.readFile(path.resolve(zipPath), { encoding: 'binary' })
-  const headers = { Authorization: `Bearer ${token}`, 'x-goog-api-version': '2' }
-  const response = await axios.put(url, body, { headers, maxContentLength: Infinity })
+  headers = { Authorization: `Bearer ${token}`, 'x-goog-api-version': '2' }
+  response = await axios.put(url, body, { headers, maxContentLength: Infinity })
+  uploadState = response.data.uploadState
 
   core.debug('Response status code: ' + response.status)
   core.debug(JSON.stringify(response.data))
-  core.info('Extension package updated.')
+
+  // Wait until package uploaded.
+  headers = {
+    Authorization: `Bearer {$token}`,
+    'x-goog-api-version': '2',
+    'Content-Length': '0',
+    'Expect': ''
+  }
+  url = `https://www.googleapis.com/chromewebstore/v1.1/items/${extId}?projection=DRAFT`
+  while (uploadState === 'IN_PROGRESS') {
+    core.info('Package is still uploading. Wait for 10 seconds.')
+    await new Promise(res => setTimeout(res, 10000))
+
+    response = await axios(url, headers)
+    uploadState = response.data.uploadState
+  }
+
+  if (uploadState === 'SUCCESS') {
+    core.info('Extension package updated.')
+    return
+  }
+
+  core.error('Failed to update extension package.')
+  core.error(JSON.stringify(response.data))
+  process.exit(1)
 }
 
 async function publishExtension(extId: string, testerOnly: boolean, token: string): Promise<void> {
@@ -50,18 +84,27 @@ async function publishExtension(extId: string, testerOnly: boolean, token: strin
     'x-goog-api-version': '2',
     'Content-Length': '0'
   }
-  const response = await axios.post(url, null, { headers, params: { target } })
+  const response: AxiosResponse<ItemPublishResponseData> = await axios.post(url, null, { headers, params: { target } })
 
   core.debug('Response status code: ' + response.status)
   core.debug(JSON.stringify(response.data))
-  core.info('Extension published.')
+
+  const status = response.data.status
+  if (status.length === 1 && status[0] === 'OK') {
+    core.info('Extension published.')
+    return
+  }
+
+  core.error('Failed to publish extension: ' + status.join(', '))
+  response.data.statusDetail.forEach(msg => core.error(msg))
+  process.exit(1)
 }
 
 async function run(extensionId: string, zipPath: string, testerOnly: boolean, clientId: string, clientSecret: string, refreshToken: string): Promise<void> {
   core.info('Start to publish extension to Web Store.')
 
   const jwtToken = await generateJwtToken(clientId, clientSecret, refreshToken)
-  await updateExtension(extensionId, zipPath, jwtToken)
+  await updatePackage(extensionId, zipPath, jwtToken)
   await publishExtension(extensionId, testerOnly, jwtToken)
 
   core.info('Extension published successfully.')

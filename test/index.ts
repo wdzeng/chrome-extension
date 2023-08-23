aa096e0dda989440ab6ccc7081b7d6521ba77132
hyperbola <me@hyperbola.me>
2023-08-24 02:48:13 +0800

    ci/cd: update workflow for unittest

--- test/index.ts
+++ test/index.ts
@@ -13,7 +13,7 @@ async function main(): Promise<void> {
   const clientSecret = requireEnvironmentVariable('TEST_CLIENT_SECRET')
   const refreshToken = requireEnvironmentVariable('TEST_REFRESH_TOKEN')
   const extensionId = requireEnvironmentVariable('TEST_EXTENSION_ID')
-  const zipPath = requireEnvironmentVariable('TEST_ZIP_PATH')
+  const zipPath = 'test/test-extension.zip'
 
   const jwtToken = await generateJwtToken(clientId, clientSecret, refreshToken)
   const success = await updatePackage(extensionId, zipPath, jwtToken)

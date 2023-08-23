617c97415c5493308ec25bb21585392c57a34ca2
hyperbola <me@hyperbola.me>
2023-08-24 02:47:56 +0800

    test: add unittest
    Also remove redundant options in tsconfig.json.

---
+++ test/index.ts
@@ -0,0 +1,23 @@
+import { generateJwtToken, updatePackage } from '@/chrome-store-utils'
+
+function requireEnvironmentVariable(key: string): string {
+  const value = process.env[key]
+  if (!value) {
+    throw new Error(`Environment variable ${key} is required.`)
+  }
+  return value
+}
+
+async function main(): Promise<void> {
+  const clientId = requireEnvironmentVariable('TEST_CLIENT_ID')
+  const clientSecret = requireEnvironmentVariable('TEST_CLIENT_SECRET')
+  const refreshToken = requireEnvironmentVariable('TEST_REFRESH_TOKEN')
+  const extensionId = requireEnvironmentVariable('TEST_EXTENSION_ID')
+  const zipPath = requireEnvironmentVariable('TEST_ZIP_PATH')
+
+  const jwtToken = await generateJwtToken(clientId, clientSecret, refreshToken)
+  const success = await updatePackage(extensionId, zipPath, jwtToken)
+  process.exit(success ? 0 : 1)
+}
+
+void main()
